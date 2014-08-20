4310be56e62e50a7d96993ac03b47a83cf1a55a6
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2014-08-21 00:23:55 +0300

    Create attributes with proper types

--- lib/cctalk/message.rb
+++ lib/cctalk/message.rb
@@ -1,2 +1,11 @@
 class Message
+  attr_accessor :destination, :length, :source, :payload, :sigmode
+
+  def initialize(data='', source=1, destination=2, header=0, payload='')
+    @source = source
+    @destination = destination
+    @payload = Payload.new(header, payload)
+    @length = payload.length
+    @sigmode = 0
+  end
 end
--- lib/cctalk/payload.rb
+++ lib/cctalk/payload.rb
@@ -1,2 +1,8 @@
 class Payload
+  attr_accessor :header, :data
+
+  def initialize(header=0, data='')
+    @header = header
+    @data = data
+  end
 end
