536e652dc9e8fb3d53691fa4317bd70c140e2abe
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2014-08-21 00:10:28 +0300

    Add basic attribute tests

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Message do
-  pending "wait for it!"
+  before { @message = Message.new }
+
+  describe 'attributes' do
+    it { expect(@message).to respond_to :destination }
+    it { expect(@message).to respond_to :length }
+    it { expect(@message).to respond_to :source }
+    it { expect(@message).to respond_to :payload }
+    it { expect(@message).to respond_to :sigmode }
+  end
+
+  describe 'attribute types' do
+    it { expect(@message.destination).to be_kind_of Fixnum }
+    it { expect(@message.length).to be_kind_of Fixnum }
+    it { expect(@message.source).to be_kind_of Fixnum }
+    it { expect(@message.payload).to be_instance_of Payload }
+    it { expect(@message.sigmode).to be_kind_of Fixnum }
+  end
+
 end
--- spec/models/payload_spec.rb
+++ spec/models/payload_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Payload do
-	pending "wait for it!"
+	before { @payload = Payload.new }
+
+	describe 'attributes' do
+    it { expect(@payload).to respond_to :header }
+    it { expect(@payload).to respond_to :data }
+  end
+
+  describe 'attribute types' do
+    it { expect(@payload.header).to be_kind_of Fixnum }
+    it { expect(@payload.data).to be_kind_of String }
+  end
+
 end
