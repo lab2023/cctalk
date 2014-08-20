7b42e39090cf042face39f7add37e585e4020179
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2014-08-21 00:39:35 +0300

    Prevent header from being non-integer or negative

--- lib/cctalk/payload.rb
+++ lib/cctalk/payload.rb
@@ -1,8 +1,9 @@
 class Payload
-  attr_accessor :header, :data
+  attr_accessor :header, :data, :header_type
 
   def initialize(header=0, data='')
-    @header = header
+    @header = header.to_i >= 0 ? header.to_i : 0
     @data = data
+    @header_type = ''
   end
 end
