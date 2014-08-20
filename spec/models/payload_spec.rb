835774d8dd729279a00a9184c03ab4db5a0a02bc
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2014-08-21 00:35:09 +0300

    Add new tests for Payload attributes

--- spec/models/payload_spec.rb
+++ spec/models/payload_spec.rb
@@ -6,11 +6,25 @@ describe Payload do
 	describe 'attributes' do
     it { expect(@payload).to respond_to :header }
     it { expect(@payload).to respond_to :data }
+    it { expect(@payload).to respond_to :header_type }
   end
 
   describe 'attribute types' do
     it { expect(@payload.header).to be_kind_of Fixnum }
     it { expect(@payload.data).to be_kind_of String }
+    it { expect(@payload.header_type).to be_kind_of String }
+  end
+
+  describe 'header' do
+    it 'should use default value' do
+      @payload = Payload.new 'asdfg', 'data'
+      expect(@payload.header).to equal 0
+    end
+
+    it 'should not be negative' do
+      @payload = Payload.new -125, 'data'
+      expect(@payload.header).to equal 0
+    end
   end
 
 end
